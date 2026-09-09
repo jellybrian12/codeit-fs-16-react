02ce2482ead2f008e1e970c524e656df4bfe9d7e
jellybrian12 <rkdwotjd1017@gmail.com>
2026-09-09 14:35:43 +0900

    1

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import { useState } from "react";
 import page from './components/FeedPage.module.scss';
-import Stories from './lab/p2-07/practice3.jsx';
-// import Stories from './components/Stories.jsx';
+// import Stories from './lab/p2-07/practice3.jsx';
+import Stories from './components/Stories.jsx';
 import FeedList from "./components/FeedList.jsx";
 
-let initialPosts = [
+const initialPosts = [
   {
     id: 1,
     username: 'jaehoon',
